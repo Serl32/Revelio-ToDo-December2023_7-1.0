fca9844e43960cec4c46eef278c3fe0af72a5e55
serl32 <s8532767@gmail.com>
2023-12-07 23:21:19 +0200

    Add contents in program

--- Revelio-ToDo-December2023_7-1.0/Program.cs
+++ Revelio-ToDo-December2023_7-1.0/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Revelio - ToDo Program Version - 1.0
+
+const string version = "1.0";
+
+DateTime dateTime = DateTime.Now;
+
+Console.WriteLine("\tRevelio ToDo Version - {0}\t\t\t{1}", version, dateTime.ToString());
+
+string? inputCommand;
+
+do
+{
+    inputCommand = Console.ReadLine();
+    if (inputCommand == null )
+    {
+        Console.WriteLine("NULL Command detected.");
+        return;
+    }
+
+    switch(inputCommand)
+    {
+        case "add":
+            break;
+        case "complete":
+            break;
+        case "create":
+            break;
+        case "help":
+            break;
+        default:
+            Console.WriteLine("Unknown command detected.");
+            Console.WriteLine("Please Enter a valid Command.");
+            break;
+    }
+} while (!inputCommand.Equals("exit"));
